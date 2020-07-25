0cb04e8d949300e945c55f6782ebb8786b21a468
Dmitriy <kozhanov.dima83@yandex.ru>
2020-07-25 13:23:23 +0300

    first task

--- script/script.js
+++ script/script.js
@@ -1,1 +1,19 @@
-console.log('!!!!');
+'use strict'
+
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели', '');
+let personalMovieDB = {
+          count: +numberOfFilms,
+          movies: {},
+          actors: {},
+          genres: [],
+          privat: false
+      };
+
+let lastMovieWatched_1 = prompt('Один из последних просмотренных фильмов?', ''),
+    scoreMovie_1 = prompt('На сколько оцените его?', ''),   
+    lastMovieWatched_2 = prompt('Один из последних просмотренных фильмов?', ''),
+    scoreMovie_2 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[lastMovieWatched_1] = scoreMovie_1;
+personalMovieDB.movies[lastMovieWatched_2] = scoreMovie_2;
+console.log(personalMovieDB);
