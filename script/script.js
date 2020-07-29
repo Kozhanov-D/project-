ab40b9b6af478cc568ca335429dacbba9ede84dd
Dmitriy <kozhanov.dima83@yandex.ru>
2020-07-29 20:29:27 +0300

    function part

--- script/script.js
+++ script/script.js
@@ -1,19 +1,70 @@
-'use strict'
+"use strict";
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        //numberOfFilms = numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+        start();
+    }
+}
+
+start();
+
 let personalMovieDB = {
-          count: +numberOfFilms,
-          movies: {},
-          actors: {},
-          genres: [],
-          privat: false
-      };
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false 
+};
 
-let lastMovieWatched_1 = prompt('Один из последних просмотренных фильмов?', ''),
-    scoreMovie_1 = prompt('На сколько оцените его?', ''),   
-    lastMovieWatched_2 = prompt('Один из последних просмотренных фильмов?', ''),
-    scoreMovie_2 = prompt('На сколько оцените его?', '');
+function detectPersonalLevel() {
+    if (0 < personalMovieDB.count && personalMovieDB.count < 10) {
+        console.log('Просмотренно довольно мало фильмов');
+    } else if (10 <= personalMovieDB.count && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
 
-personalMovieDB.movies[lastMovieWatched_1] = scoreMovie_1;
-personalMovieDB.movies[lastMovieWatched_2] = scoreMovie_2;
-console.log(personalMovieDB);
+detectPersonalLevel();
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let lastMovieWatched = prompt('Один из последних просмотренных фильмов?', ''),
+            scoreMovie = prompt('На сколько оцените его?', '');
+
+        if (lastMovieWatched != null && scoreMovie != null && lastMovieWatched != '' && scoreMovie != '' && lastMovieWatched.length < 50) {
+            personalMovieDB.movies[lastMovieWatched] = scoreMovie;
+            console.log('Done');
+        } else {
+            console.log('Error');
+            i--;
+        } 
+    }
+}
+
+rememberMyFilms();
+
+function showMyBd() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    }
+}
+
+function writeYourGenres() {
+    for (let i = 1; i <=3; i++) {
+        let yuorGener = prompt(`Ваш любимый жанр под номером ${i}`, '');
+        personalMovieDB.genres.push(yuorGener);
+    }
+}
+
+writeYourGenres();
+
+showMyBd();
